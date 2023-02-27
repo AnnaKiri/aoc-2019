122fd01394b578aed4378fde7e1f1098360a498c
Anna Kirillova <a.kirillova1812@gmail.com>
2023-02-27 18:32:24 +0400

    Part 2 with recursion was added

---
+++ src/Day1/Part2Recursion.java
@@ -0,0 +1,46 @@
+package Day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part2Recursion {
+	public static void main(String[] args) {
+		String path = ".\\src\\Day1\\data.txt";
+		File file = new File(path);
+		List<Integer> modules = new ArrayList<>();
+		
+		try {
+			Scanner scan = new Scanner(file);
+			
+			while (scan.hasNextInt()) {
+				modules.add(scan.nextInt());
+			}
+			
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		
+			int result = 0;
+			for (int i = 0; i < modules.size(); i++) {
+				int module = modules.get(i);
+
+				result += findFuel(module);
+			}
+			
+			System.out.println(result);
+
+	}
+
+	private static int findFuel(int mass) {
+		int fuel = (int) Math.floor(mass / 3) - 2;
+
+		if (fuel <=0)
+			return 0;
+
+		return fuel + findFuel(fuel);
+	}
+}
